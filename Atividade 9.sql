b21e58545121725314822bae99c006e32183eb69
Matheus Henrique <matheushenriquebr88@gmail.com>
2025-12-15 20:23:24 -0300

    Add data to Cursos and Alunos tables
    Inserted values into Cursos and Alunos tables.

--- Atividade 9.sql
+++ Atividade 9.sql
@@ -31,6 +31,19 @@ create table Cursos (
 alter table Cursos
 ADD column preco decimal(10,2);
 
+/*Inserir valores na tabela*/
+insert into cursos(nome_curso, preco)
+values
+("Engenharia Da Computação", 3518.10),
+("Análise Desenvolvimento De Sistemas", 2758),
+("Ciência Da Computação", 1800);
+
+insert into Alunos(id_aluno, nome, data_nasc)
+values
+(12, "Maikon","2000-02-25"),
+(32, "Lanaildo", "1999-03-19"),
+(16, "Junielson", "1988-12-12"); 
+
 
 
 
